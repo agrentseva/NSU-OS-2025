e6cbcd59299a49639d1187a2d8949314e76760ef
agrentseva <a.grentseva@g.nsu.ru>
2025-09-20 11:49:51 +0700

    laba3

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <unistd.h>
+
+void printID();
+void fileOpen(char* filename);
+
+int main(int argc, char* argv[]){
+
+    if (argc != 2){
+        fprintf(stderr, "%s\n", "Enter the filename");
+        return 1;
+    }
+
+    printf("Without setuid:\n");
+    printID();
+
+    fileOpen(argv[1]);
+    if (setuid(geteuid()) == -1){
+        perror("Fail setuid");
+        return 1;
+    }
+
+    printf("With setuid:\n");
+    printID();
+
+    fileOpen(argv[1]);
+    return 0;
+}
+
+void printID(){
+    uid_t real = getuid();
+    uid_t effective = geteuid();
+
+    printf("Real user ID: %d\n", real);
+    printf("Effective user ID: %d\n", effective);
+}
+
+void fileOpen(char* filename){
+    FILE* file;
+    if (!(file = fopen(filename, "r"))){
+        perror("Coudn't open file");
+        return;
+    }
+    if (fclose(file)) {
+        perror("Couldn't close file");
+        return;
+    }
+}
